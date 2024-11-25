5d27b7d33ba03478d35665c2780d2ae1a97007ff
Vatsal1204 <pitrodavatsal1242005@gmail.com>
2024-11-25 10:15:53 +0530

    back

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if (a>b){
+        printf("a is greater than b",a,b);
+    }
+    else
+    {
+        printf("b is greater than a",b,a);
+    }
+}
